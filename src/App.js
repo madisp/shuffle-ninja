eac82f4784c20f23564817e2d078937f3b15a2dd
Madis Pink <madis.pink@gmail.com>
2020-06-28 15:22:37 +0300

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { SpotifyApiContext } from 'react-spotify-api';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Cover">
+        ???
+      </div>
+      <div>
+        <p>Sign in with Spotify to reveal the album.</p>
+      </div>
     </div>
   );
 }
